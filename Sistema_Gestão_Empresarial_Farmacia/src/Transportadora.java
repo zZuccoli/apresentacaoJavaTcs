e252ab2a646232da0b3ffc4ce44df6970d47b4f0
Igor Duim Moro <140767577+IgorDMoro@users.noreply.github.com>
2025-06-10 12:24:05 -0300

    Add info na Main

--- Sistema_Gestão_Empresarial_Farmacia/src/Transportadora.java
+++ Sistema_Gestão_Empresarial_Farmacia/src/Transportadora.java
@@ -3,15 +3,15 @@ public class Transportadora {
 	private String nome;
 	private int cnpj;
 	private String localAtendimento;
-	
+
 	public Transportadora() {
-		
+
 	}
-	
+
 	public Transportadora(String nome, int cnpj, String localAtendimento) {
-		this.nome = nome;
-		this.cnpj = cnpj;
-		this.localAtendimento = localAtendimento;
+		setNome(nome);
+		setCnpj(cnpj);
+		setLocalAtendimento(localAtendimento);
 	}
 
 	public String getNome() {
@@ -19,6 +19,13 @@ public class Transportadora {
 	}
 
 	public void setNome(String nome) {
+		try {
+			if (nome.trim().isEmpty()) {
+				throw new Exception("Nome não pode ser vazio");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		this.nome = nome;
 	}
 
@@ -26,16 +33,28 @@ public class Transportadora {
 		return cnpj;
 	}
 
-	public void setCnpj(int id) {
-		this.cnpj = id;
+	public void setCnpj(int cnpj) {
+		try {
+			if (cnpj < 0 || cnpj > 99999999999999L) {
+				throw new Exception("CNPJ Inválido");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		this.cnpj = cnpj;
 	}
-
 	public String getLocalAtendimento() {
 		return localAtendimento;
 	}
 
 	public void setLocalAtendimento(String localAtendimento) {
+		try {
+			if (localAtendimento.trim().isEmpty()) {
+				throw new Exception("Local de atendimento não pode ser vazio");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		this.localAtendimento = localAtendimento;
 	}
-	
 }
